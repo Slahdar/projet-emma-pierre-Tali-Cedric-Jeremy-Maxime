4894f8131f5de58e0a9482baf729a9211d1969cb
maxime_chostak <mchostak@yahoo.fr>
2023-04-06 09:04:47 +0200

    V2, merge de la navbar et des autres pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,110 +1,201 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // const headerContainer = document.getElementById("header-container");
-  // headerContainerrr.innerHTML = `
-  //   <div id="flex-1">
-  //   <nav class="menu-left">
-  //   <i class="fa-solid fa-phone"></i><span> +33 7 65 78 65 43</span>
-  //   <i class="fa-solid fa-envelope"></i><span>  test@test.fr</span>
-  //   </nav>
-  //   <div class="logo-container">
-  //     <img src="image.png" class="logo">
-  //   </div>
-  //   <nav class="menu-right">
-  //     <div class="icon-container">
-  //       <i class="fa-solid fa-magnifying-glass"></i>
-  //       <i class="fa-regular fa-user"></i>
-  //       <i class="fa-solid fa-cart-shopping"></i>
-  //     </div>
-  //   </nav>
-  //   </div>
-  //   <div id="flex-2">
-  //     <nav id="menu">
-  //         <p>item1</p>
-  //         <p>item1</p>
-  //         <p>item1</p>
-  //         <p>item1</p>
-  //     </nav>
-  //   </div>
-  //   <div id="flex-3">
+document.addEventListener('DOMContentLoaded', function() {
 
-  //   </div>
 
-  // `;
+  const headerContainer = document.getElementById("header-container");
+headerContainer.innerHTML = `
+<div id="block-1">
+<nav class="menu-left" id="head-cred" >
+<i class="fa-solid fa-phone"></i><span> +33 7 65 78 65 43</span>
+<i class="fa-solid fa-envelope"></i><span>  test@test.fr</span>
+</nav>
+<div id="head-burger">
+  <i class="fa-solid fa-bars" id="burger"></i>
+</div>  
+<div class="logo-container">
+<a href="index.html">
+  <img src="https://cdn.discordapp.com/attachments/1092361701158043738/1092399129960914964/EP-logo-DEF-carre.png" class="logo">
+</a>
+  </div>
+<nav class="menu-right">
 
-  const firstDiv = document.getElementById("first-div");
-  const secondDiv = document.getElementById("block-3");
+  <div class="icons-container">
+    <div class="icon-container">
+        
+        <i class="fa-solid fa-magnifying-glass"></i>
+    </div>
 
-  // Hide the second div initially
-  secondDiv.style.display = "none";
+    <div class="icon-container">
+        
+        <i class="fa-regular fa-user"></i>
+    </div>
 
-  // Add a mouseenter event listener to the first div
-  firstDiv.addEventListener("mouseenter", function () {
-    // Show the second div
-    console.log("hovered");
-    secondDiv.style.display = "flex";
-  });
+    <div class="icon-container">
+        
+        <i id="cart-btn" class="fa-solid fa-cart-shopping"></i>
+    </div>
+    
+    
+    
+    <div id="cart">
+        <div id="cart-items">
+            <div class="cart-item"></div>
+            <div class="cart-item"></div>
+            <div class="cart-item"></div>
+        </div>
+        <div id="check-out">
+            <button id="check-out-btn">CHECK OUT</button>
+        </div>
+    </div>
+  </div>
+</nav>
+</div>
+<div id="block-2">
+  <nav id="menu">
+      <a href="a-propos.html" >A propos</a>
+      <a href="product_list.html" id="first-div">Nos Collections</a>
+      <div id="block-3">
+        <div class="items"></div>
+        <div class="items"></div>
+        <div class="items"></div>
+        </div>
+      <a href="blog.html">Blog</a>
+      <a href="contact.html">Contact</a>
+  </nav>
+</div>
 
-  // Add a mouseleave event listener to the first div
-  firstDiv.addEventListener("mouseleave", function () {
-    // Check if the mouse is hovering over the second div
-    const isHovering = isMouseHovering(secondDiv);
+  `;
 
-    // If the mouse is not hovering over the second div, hide it
-    if (!isHovering) {
-      secondDiv.style.display = "none";
-    }
-  });
 
-  // Add a mouseenter event listener to the second div
-  secondDiv.addEventListener("mouseenter", function () {
-    // Show the second div
-    secondDiv.style.display = "flex";
-  });
 
-  // Add a mouseleave event listener to the second div
-  secondDiv.addEventListener("mouseleave", function () {
-    // Hide the second div
-    secondDiv.style.display = "none";
-  });
 
-  // Helper function to check if the mouse is hovering over an element
-  function isMouseHovering(element) {
-    const { top, left, bottom, right } = element.getBoundingClientRect();
-    const { clientX, clientY } = event;
+ const firstDiv = document.getElementById("first-div");
+ const secondDiv = document.getElementById("block-3");
 
-    return (
-      clientX >= left && clientX <= right && clientY >= top && clientY <= bottom
-    );
-  }
+ // Hide the second div initially
+ secondDiv.style.display = "none";
 
-  const toggleDiv = document.getElementById("cart-btn");
-  const otherDiv = document.getElementById("cart");
+ // Add a mouseenter event listener to the first div
+ firstDiv.addEventListener("mouseenter", function() {
+   // Show the second div
+   console.log('hovered')
+   secondDiv.style.display = "flex";
+ });
 
-  // Hide the other div initially
-  otherDiv.style.display = "none";
+ // Add a mouseleave event listener to the first div
+ firstDiv.addEventListener("mouseleave", function() {
+   // Check if the mouse is hovering over the second div
+   const isHovering = isMouseHovering(secondDiv);
 
-  // Add a click event listener to the toggle div
-  toggleDiv.addEventListener("click", function () {
-    // Toggle the display of the other div
-    if (otherDiv.style.display === "none") {
-      otherDiv.style.display = "block";
-    } else {
-      otherDiv.style.display = "none";
-    }
-  });
+   // If the mouse is not hovering over the second div, hide it
+   if (!isHovering) {
+     secondDiv.style.display = "none";
+   }
+ });
 
-  const cursor = document.getElementById("cursor");
+ // Add a mouseenter event listener to the second div
+ secondDiv.addEventListener("mouseenter", function() {
+   // Show the second div
+   secondDiv.style.display = "flex";
+ });
 
-  document.addEventListener("mousemove", (e) => {
-    cursor.style.top = e.pageY + "px";
-    cursor.style.left = e.pageX + "px";
-  });
+ // Add a mouseleave event listener to the second div
+ secondDiv.addEventListener("mouseleave", function() {
+   // Hide the second div
+   secondDiv.style.display = "none";
+ });
 
-  document.addEventListener("mouseleave", (e) => {
-    cursor.style.display = "none";
-  });
+ // Helper function to check if the mouse is hovering over an element
+ function isMouseHovering(element) {
+   const { top, left, bottom, right } = element.getBoundingClientRect();
+   const { clientX, clientY } = event;
 
-  document.addEventListener("mouseenter", (e) => {
-    cursor.style.display = "block";
-  });
+   return clientX >= left && clientX <= right && clientY >= top && clientY <= bottom;
+ }
+
+
+ const toggleDiv = document.getElementById("cart-btn");
+ const otherDiv = document.getElementById("cart");
+
+
+ // Hide the other div initially
+ otherDiv.style.display = "none";
+
+
+ // Add a click event listener to the toggle div
+ toggleDiv.addEventListener("click", function() {
+   // Toggle the display of the other div
+   if (otherDiv.style.display === "none") {
+     otherDiv.style.display = "block";
+   } else {
+     otherDiv.style.display = "none";
+   }
+ });
+
+
+ const burger = document.getElementById("burger");
+ const burgerMenu = document.getElementById("block-2");
+
+
+
+
+
+ // Add a click event listener to the toggle div
+ burger.addEventListener("click", function() {
+   // Toggle the display of the other div
+   if (burgerMenu.style.display === "none") {
+     burgerMenu.style.display = "flex";
+   } else {
+     burgerMenu.style.display = "none";
+   }
+ });
+
+
+
+
+//   const cursor = document.getElementById('cursor');
+
+
+//   if (window.innerWidth < 747) {
+//     cursor.style.display = "none";
+//   }
+
+
+//   else{
+
+
+
+// document.addEventListener('mousemove', e => {
+//   cursor.style.top = e.pageY + 'px';
+//   cursor.style.left = e.pageX + 'px';
+// });
+
+
+// document.addEventListener('mouseleave', e => {
+//   cursor.style.display = 'none';
+// });
+
+
+// document.addEventListener('mouseenter', e => {
+//   cursor.style.display = 'block';
+// });
+
+
+// }
+
+
+
+
+window.addEventListener('scroll', function() {
+ var navbar = document.getElementById('header-container');
+ if (window.pageYOffset > 20) {
+   navbar.style.backgroundColor = 'white';
+  
+ }
+ if (window.pageYOffset === 0) {
+   navbar.style.backgroundColor = '#ffffff87';
+  
+ }
 });
+});
+
+
