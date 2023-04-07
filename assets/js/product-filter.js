80e4763839ef41355e0703fe4964a34dcd88dbfe
cedric dousset <cedricdsst@gmail.com>
2023-04-07 09:02:49 +0200

    modification filtres pas prod-list et correction liens

--- assets/js/product-filter.js
+++ assets/js/product-filter.js
@@ -1,23 +1,29 @@
 const filterTitles = document.querySelectorAll('.filter-title');
 
 window.onload = function() {
-filterTitles.forEach(filterTitle => {
-  filterTitle.addEventListener('click', () => {
+  filterTitles.forEach(filterTitle => {
     const parent = filterTitle.parentNode;
 
-    if (parent.classList.contains('active-filter')) {
-      parent.classList.remove('active-filter');
-      parent.style.height = '40px';
-      filterTitle.querySelector('.fa-solid').style.transform = 'rotate(0deg)';
-    } else {
-      parent.classList.add('active-filter');
-      parent.style.height = 'auto';
-      filterTitle.querySelector('.fa-solid').style.transform = 'rotate(180deg)';
-    }
-  });
-});
+    // set initial state to active
+    parent.classList.add('active-filter');
+    parent.style.height = 'auto';
 
-}
+    // rotate icon on load
+    filterTitle.querySelector('.fa-solid').style.transform = 'rotate(180deg)';
+
+    filterTitle.addEventListener('click', () => {
+      if (parent.classList.contains('active-filter')) {
+        parent.classList.remove('active-filter');
+        parent.style.height = '40px';
+        filterTitle.querySelector('.fa-solid').style.transform = 'rotate(0deg)';
+      } else {
+        parent.classList.add('active-filter');
+        parent.style.height = 'auto';
+        filterTitle.querySelector('.fa-solid').style.transform = 'rotate(180deg)';
+      }
+    });
+  });
+};
 
 const gridWrapper = document.querySelector('.grid-wrapper');
 const childDivs = gridWrapper.querySelectorAll(':scope > div');
@@ -32,9 +38,6 @@ function removeClasses() {
 
 // Call removeClasses initially
 removeClasses();
-
-// Call removeClasses on window resize
-window.addEventListener('resize', removeClasses);
 
 
 
